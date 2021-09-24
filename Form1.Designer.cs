80cd05d1ec9818844abc062dd7bd52a7bb026c79
JOSUE ALEXANDER HERRERA ORTIZ <20190158@unapec.edu.do>
2021-09-24 19:10:00 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ProgramaA_Lectura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Generar = new System.Windows.Forms.Button();
+            this.Btn_Leer = new System.Windows.Forms.Button();
+            this.Txt_Archivo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_Generar
+            // 
+            this.Btn_Generar.Location = new System.Drawing.Point(881, 1);
+            this.Btn_Generar.Name = "Btn_Generar";
+            this.Btn_Generar.Size = new System.Drawing.Size(120, 43);
+            this.Btn_Generar.TabIndex = 5;
+            this.Btn_Generar.Text = "Generar archivo";
+            this.Btn_Generar.UseVisualStyleBackColor = true;
+            this.Btn_Generar.Click += new System.EventHandler(this.Btn_Generar_Click);
+            // 
+            // Btn_Leer
+            // 
+            this.Btn_Leer.Location = new System.Drawing.Point(12, 1);
+            this.Btn_Leer.Name = "Btn_Leer";
+            this.Btn_Leer.Size = new System.Drawing.Size(120, 43);
+            this.Btn_Leer.TabIndex = 6;
+            this.Btn_Leer.Text = "Leer Archivo";
+            this.Btn_Leer.UseVisualStyleBackColor = true;
+            this.Btn_Leer.Click += new System.EventHandler(this.Btn_Leer_Click_1);
+            // 
+            // Txt_Archivo
+            // 
+            this.Txt_Archivo.Location = new System.Drawing.Point(12, 50);
+            this.Txt_Archivo.Multiline = true;
+            this.Txt_Archivo.Name = "Txt_Archivo";
+            this.Txt_Archivo.Size = new System.Drawing.Size(989, 171);
+            this.Txt_Archivo.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 230);
+            this.Controls.Add(this.Txt_Archivo);
+            this.Controls.Add(this.Btn_Leer);
+            this.Controls.Add(this.Btn_Generar);
+            this.Name = "Form1";
+            this.Text = "Programa A - Lectura de Archivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Btn_Generar;
+        private System.Windows.Forms.Button Btn_Leer;
+        private System.Windows.Forms.TextBox Txt_Archivo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaA_Lectura
+{
+    public partial class Form1 : Form
+    {
+        string fileName = @"C:\Users\Josue\Documents\Detalles.txt";
+        string fileName2 = @"C:\Users\Josue\Documents\Detalles2.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /* private void Btn_Leer2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Filtro.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            //openFileDialog1.Multiselect = true;
+
+            // Mostrar el dialog box.
+            openFileDialog1.ShowDialog();
+
+            string archivo = openFileDialog1.FileName;
+
+            if (archivo.Trim().Length > 0) //Validar si se selecciono o no un archivo
+                // Abrir el archivo seleccionado.
+
+                using (StreamReader sr = new StreamReader(archivo))
+                {
+                    String line = sr.ReadToEnd();
+                    Txt_Archivo.Text = line;
+                }
+        }*/
+
+
+
+        private void writeFileLine(string pLine)
+        {
+            /*using (StreamWriter w = File.AppendText(fileName))*/
+            using (StreamWriter w = File.AppendText(fileName2))
+            {
+                w.WriteLine(pLine);
+            }
+        }
+
+        private void Btn_Generar_Click(object sender, EventArgs e)
+        {
+            writeFileLine("E,NV,402-1896086-3,02-2021");
+            writeFileLine("D,002,IN,24-09-2021,24-10-2021,C,402-1223124-4,Jose,Mendoza,M,11-06-1992,12000.00,150.0,12000.00,N/A");
+            writeFileLine("D,002,IN,24-09-2021,24-10-2021,C,402-1291636-4,Maria,Soza,F,04-10-1991,69000.00,250.0,69000.00,N/A");
+            writeFileLine("S,4");
+            MessageBox.Show("Archivo generado con exito");
+        }
+
+        private void Btn_Leer_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName2))
+                {
+                    String line = sr.ReadToEnd();
+                    Txt_Archivo.Text = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + ex.Message);
+            }
+        }
+    }
+}
